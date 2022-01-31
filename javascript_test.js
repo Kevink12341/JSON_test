97b7629b67ac14ee7627b14cce9d2456581ee89c
Kevink12341 <77891062+Kevink12341@users.noreply.github.com>
2022-01-31 11:36:31 +0100

    Update javascript_test.js

--- javascript_test.js
+++ javascript_test.js
@@ -1,29 +1,42 @@
 let cbs_link = "https://opendata.cbs.nl/ODataApi/odata/70072ned/UntypedDataSet?$filter=((substringof(%27NL%27,RegioS)))&$select=Perioden,+TotaleBevolking_1";
 let cbs_link2 = "https://odata4.cbs.nl/CBS/83878NED";
 
-
+// Data laden
 async function load_data() {
     const response = await fetch(cbs_link);
     const jsondata = await response.json();
-    data = jsondata.value
-    drawTable(data);
+    let data = jsondata.value;
+    addTable(data);
 }
-data = load_data();
-// fetch(cbs_link)
-//     .then(response => response.json())
-//         .then(data => data.value
-// );
+const data = load_data();
 
+// Tabel generen
+function addTable(data) {
 
-function drawTable(data) {
-    for (var i = 0; i < data.length; i++) {
-    drawRow(data[i]);
+const myTableDiv = document.getElementById("myDynamicTable");
+    
+const table = document.createElement('TABLE');
+table.border='1';
+
+const tableBody = document.createElement('TBODY');
+table.appendChild(tableBody);
+
+// Loop voor de data in de tabel
+    
+for (var i=0; i < data.length; i++){
+    var tr = document.createElement('TR');
+    tableBody.appendChild(tr);
+    
+    for (var j=0; j< 1; j++){
+        var td = document.createElement('TD');
+        td.appendChild(document.createTextNode(data[i].Perioden.slice(0,4)));
+        tr.appendChild(td);
+        td = document.createElement('TD');
+        td.appendChild(document.createTextNode(data[i].TotaleBevolking_1));
+        tr.appendChild(td);
     }
 }
+myTableDiv.appendChild(table);
+}
 
-function drawRow(rowData) {
-    var row = $("<tr />")
-    $("#table-id").append(row);
-    row.append($("<td>" + rowData.Perioden.substring(0, 4) + "</td>"));
-    row.append($("<td>" + rowData.TotaleBevolking_1 + "</td>"));
-  }
+console.log(data[0]);
